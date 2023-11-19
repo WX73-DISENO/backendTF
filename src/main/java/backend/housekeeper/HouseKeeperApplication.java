0ac8590348bffc884af0a0791c1bdc86a5f0c225
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-18 21:28:30 -0500

    Initial commit

---
+++ src/main/java/backend/housekeeper/HouseKeeperApplication.java
@@ -0,0 +1,13 @@
+package backend.housekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseKeeperApplication.class, args);
+	}
+
+}

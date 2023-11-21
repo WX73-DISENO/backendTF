1736259fe4aed470cfdab252cfb1787ea2dfc47d
Alonso Robles <alonsofernando1907@outlook.es>
2023-11-21 08:27:18 -0500

    Update GetHouseAllQuery.java

--- src/main/java/backend/housekeeper/houseelegantservice/domain/model/queries/GetHouseAllQuery.java
+++ src/main/java/backend/housekeeper/houseelegantservice/domain/model/queries/GetHouseAllQuery.java
@@ -1,4 +1,8 @@
 package backend.housekeeper.houseelegantservice.domain.model.queries;
 
+<<<<<<<< HEAD:src/main/java/backend/housekeeper/houseelegantservice/domain/model/queries/GetHouseAllQuery.java
 public record GetHouseAllQuery() {
+========
+public record GetKeeperAllQuery() {
+>>>>>>>> Leonardo-Patrocinio:src/main/java/backend/housekeeper/houseelegantservice/domain/model/queries/GetKeeperAllQuery.java
 }
